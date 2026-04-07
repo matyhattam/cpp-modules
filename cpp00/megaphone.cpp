362eaf7f6e2deb51a9d1e3772a28e67857bffe50
maty <maty.hattam@gmail.com>
2026-04-07 12:25:09 +0700

    megaphone: Done

--- cpp00/megaphone.cpp
+++ cpp00/megaphone.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <string>
 
-int ft_strlen(std::string str) {
-  int i = 0;
-
-  while (str[i])
-    i++;
-
-  return i;
-}
-
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << "LOUD AND UNBEARABLE FEEDBACK \n";
+    std::cout << "LOUD AND UNBEARABLE FEEDBACK" << std::endl;
     return 0;
   }
 
-  std::string input = argv[1];
+  for (int i = 1; argv[i] != NULL; i++) {
+    for (int j = 0; argv[i][j]; j++)
+      if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+        argv[i][j] = std::toupper(argv[i][j]);
 
-  for (int i = 0; i < (int)input.length(); i++)
-    if (input[i] >= 'a' && input[i] <= 'z')
-      input[i] -= 32;
-
-  std::cout << input << '\n';
+    std::cout << argv[i];
+  }
+  std::cout << std::endl;
 
   return 0;
 }
