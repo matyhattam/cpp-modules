9aefaa7d8a96a2933618bc1e40297424747d9de4
maty <maty.hattam@gmail.com>
2026-04-07 22:19:32 +0700

    cleaning

--- cpp00/my_awesome_phone_book/src/main.cpp
+++ cpp00/my_awesome_phone_book/src/main.cpp
@@ -3,7 +3,8 @@
 
 int main() {
   PhoneBook pb;
+  Contact ct = Contact("maty", "hattam", "titi", "0661328874", "rien");
 
-  pb.addContact(Contact("maty", "hattam", "titi", "0661328874", "rien"));
+  pb.addContact(ct);
   pb.displayContacts();
 }
