acc779a585dd1581d05659e5931d530aff0bd430
maty <maty.hattam@gmail.com>
2026-07-01 12:36:09 +0700

    ex03 -> tests

--- cpp01/src/main.cpp
+++ cpp01/src/main.cpp
@@ -1,3 +1,6 @@
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+#include "Weapon.hpp"
 #include "Zombie.hpp"
 
 int main() {
@@ -10,12 +13,21 @@ int main() {
 
   delete zb_quentin;
 
-  Zombie *horde = zombieHorde(10, "test");
-  horde->announce();
+  // Zombie *horde = zombieHorde(10, "test");
+  // horde->announce();
 
-  for (int i = 0; i < 10; i++) {
-    horde->announce();
-  }
+  // for (int i = 0; i < 10; i++) {
+  //   horde->announce();
+  // }
 
-  delete[] horde;
+  // delete[] horde;
+
+  HumanA h_maty = HumanA("maty", Weapon());
+  h_maty.setWeaponType("hache");
+  h_maty.attack();
+
+  HumanB h_maeva = HumanB("maeva");
+  h_maeva.setWeapon(Weapon());
+  h_maeva.setWeaponType("Epée");
+  h_maeva.attack();
 }
